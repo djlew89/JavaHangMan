ecc79a2acc29e979ba3b9236c467df44068ed9aa
Dan Lewis <Daniel.Lewis37@ed.cna.nl.ca>
2022-07-30 18:58:56 -0230

    Early idea for ascii art hangman states

---
+++ src/com/danlewis/UI.java
@@ -0,0 +1,73 @@
+package com.danlewis;
+
+public class UI {
+    public static void initialState(){
+        System.out.println("--------");
+        System.out.println("|       |");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("___________");
+    }
+
+    public static void wrongGuessOne(){
+        System.out.println("--------");
+        System.out.println("|       |");
+        System.out.println("|       O");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("___________");
+    }
+
+    public static void wrongGuessTwo(){
+        System.out.println("--------");
+        System.out.println("|       |");
+        System.out.println("|       O");
+        System.out.println("|       |");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("___________");
+    }
+
+    public static void wrongGuessThree(){
+        System.out.println("--------");
+        System.out.println("|       |");
+        System.out.println("|       O");
+        System.out.println("|      /|");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("___________");
+    }
+
+    public static void wrongGuessFour(){
+        System.out.println("--------");
+        System.out.println("|       |");
+        System.out.println("|       O");
+        System.out.println("|      /|\\");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("___________");
+    }
+
+    public static void wrongGuessFive(){
+        System.out.println("--------");
+        System.out.println("|       |");
+        System.out.println("|       O");
+        System.out.println("|      /|\\");
+        System.out.println("|      /");
+        System.out.println("|");
+        System.out.println("___________");
+    }
+
+    public static void wrongGuessSix(){
+        System.out.println("--------");
+        System.out.println("|       |");
+        System.out.println("|       O");
+        System.out.println("|      /|\\");
+        System.out.println("|      / \\");
+        System.out.println("|");
+        System.out.println("___________");
+    }
+}
